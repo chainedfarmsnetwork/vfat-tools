cae74b31e09276b6f2daee0aa7ee4d2e54e36df5
Rdyx <tiranicolas.pro@gmail.com>
2021-06-03 22:23:24 +0200

    Main page

--- src/static/js/pages.js
+++ src/static/js/pages.js
@@ -16,18 +16,20 @@ function setHTMLTable(data, tableId, header = false) {
 const main = async () => {
   let tableData = {
     title: 'Chained Farms Network',
-    heading: ['Name', 'Pools', 'Token', 'Link'],
+    heading: ['Name', 'Pools', 'Token', 'Max Supply', 'Link'],
     rows: [
       [
         'The Main Farm',
         `<a href="/mainchain/main">Various</a>`,
         'CFN',
+        '1,000,000',
         '<a href="https://main.chainedfarms.network">Link</a>',
       ],
       [
         'The RollerCoaster',
         `<a href="/mainchain/sub1">Various</a>`,
         'SFN1',
+        '642,000,000',
         '<a href="https://sub1.chainedfarms.network">Link</a>',
       ],
     ],
